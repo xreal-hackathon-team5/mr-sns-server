4344de42186152bb98bc06270724bec3dcf77483
JadenChoi2k <jkya02@gmail.com>
2025-01-11 00:33:08 +0900

    edit host

--- app.py
+++ app.py
@@ -248,4 +248,4 @@ def delete_user(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
